dbed3e7ec69a8ec2e324f9a85536ddb9d4000a81
Sasidaran22 <68269150+Sasidaran22@users.noreply.github.com>
2021-01-07 20:54:50 +0530

    Initial commit - displaying mail archives

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','ucdavis.edu','uct.ac.za','columbia.edu','indiana.edu','berkeley.edu','ufp.pt','mac.com','unl.edu','mtu.edu'],
+['2005',57,11,14,13,12,12,10,12,12,11],
+['2006',1,5,0,0,1,1,3,0,0,0]
+];
